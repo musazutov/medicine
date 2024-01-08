9b28ea43c8fac68ecf8808317ba6cac011276f0d
musazutov <muzazutov@gmail.com>
2024-01-08 14:51:07 +0400

    dev branch commit

--- lib/screens/preparation_detail_screen.dart
+++ lib/screens/preparation_detail_screen.dart
@@ -9,7 +9,7 @@ import '../utils/variables.dart';
 class PreparationDetailScreen extends StatelessWidget {
   PreparationDetailScreen({Key? key, required this.preparationId})
       : super(key: key);
-///screerre dev
+///screerre dev 445645
   late PreparationDetailCubit _cubit;
   late int preparationId;
 
